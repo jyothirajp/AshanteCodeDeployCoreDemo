2b7ba11dbd0bae8ee8c6d92b599896e4e383128d
jyothirajp <49062095+jyothirajp@users.noreply.github.com>
2019-04-21 23:40:40 +0530

    Update StageArtifact.ps1

--- StageArtifact.ps1
+++ StageArtifact.ps1
@@ -31,6 +31,6 @@ function GetWebArtifactFolderPath($path)
     }
 }
 
-$path = GetWebArtifactFolderPath(CodeDeployCoreDemo\build_output)
+$path = GetWebArtifactFolderPath("C:\temp\WebApp\CodeDeployDemo")
 $path2 = $path + "\*"
 Copy-Item $path2 $target -recurse -force
