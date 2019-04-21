9044bfcfb9acf98fde902c132f006b942de5edb0
jyothirajp <49062095+jyothirajp@users.noreply.github.com>
2019-04-22 00:00:10 +0530

    Update StageArtifact.ps1

--- StageArtifact.ps1
+++ StageArtifact.ps1
@@ -12,25 +12,3 @@ function DeleteIfExistsAndCreateEmptyFolder($dir )
 # Clean up target directory
 DeleteIfExistsAndCreateEmptyFolder($target )
 
-# msdeploy creates a web artifact with multiple levels of folders. We only need the content 
-# of the folder that has Web.config within it 
-function GetWebArtifactFolderPath($path)
-{
-    foreach ($item in Get-ChildItem $path)
-    {   
-        if (Test-Path $item.FullName -PathType Container)
-        {   
-            # return the full path for the folder which contains Global.asax
-            if (Test-Path ($item.fullname + "\Global.asax"))
-            {
-                #$item.FullName
-                return $item.FullName;
-            }
-            GetWebArtifactFolderPath $item.FullName
-        }
-    }
-}
-
-$path = GetWebArtifactFolderPath("C:\temp\WebApp\CodeDeployDemo")
-$path2 = $path + "\*"
-Copy-Item $path2 $target -recurse -force
